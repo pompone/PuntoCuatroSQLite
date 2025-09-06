9e042dd14ffaadb18de9505700edbbac6d16aa6d
pompone <54503871+pompone@users.noreply.github.com>
2025-09-06 17:35:41 -0300

    Update 20250906184224_InitialCreateSqlite.Designer.cs

--- PuntoCuatroSQLite/Migrations/20250906184224_InitialCreateSqlite.Designer.cs
+++ PuntoCuatroSQLite/Migrations/20250906184224_InitialCreateSqlite.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using PuntoCuatro.Data;
+using PuntoCuatroSQLite.Data;
 
 #nullable disable
 
@@ -88,3 +88,4 @@ namespace PuntoCuatroSQLite.Migrations
         }
     }
 }
+
