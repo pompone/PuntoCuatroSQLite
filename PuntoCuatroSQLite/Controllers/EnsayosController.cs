cc0bc2de6931a5bf14e75280cc3cf995f179415a
pompone <54503871+pompone@users.noreply.github.com>
2025-09-06 16:30:00 -0300

    Quinta subida

---
+++ PuntoCuatroSQLite/Controllers/EnsayosController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PuntoCuatro.Data;
+using PuntoCuatro.Models;
+
+namespace PuntoCuatro.Controllers
+{
+    public class EnsayosController : Controller
+    {
+        private readonly LaboratorioContext _context;
+
+        public EnsayosController(LaboratorioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ensayos
+        public async Task<IActionResult> Index()
+        {
+            var laboratorioContext = _context.Ensayos.Include(e => e.Muestra);
+            return View(await laboratorioContext.ToListAsync());
+        }
+
+        // GET: Ensayos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ensayo = await _context.Ensayos
+                .Include(e => e.Muestra)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ensayo == null)
+            {
+                return NotFound();
+            }
+
+            return View(ensayo);
+        }
+
+        // GET: Ensayos/Create
+        public IActionResult Create()
+        {
+            ViewData["MuestraId"] = new SelectList(_context.Muestras, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Ensayos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MuestraId,Tipo,Resultado,Fecha")] Ensayo ensayo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ensayo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MuestraId"] = new SelectList(_context.Muestras, "Id", "Nombre", ensayo.MuestraId);
+            return View(ensayo);
+        }
+
+        // GET: Ensayos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ensayo = await _context.Ensayos.FindAsync(id);
+            if (ensayo == null)
+            {
+                return NotFound();
+            }
+            ViewData["MuestraId"] = new SelectList(_context.Muestras, "Id", "Nombre", ensayo.MuestraId);
+            return View(ensayo);
+        }
+
+        // POST: Ensayos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MuestraId,Tipo,Resultado,Fecha")] Ensayo ensayo)
+        {
+            if (id != ensayo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ensayo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EnsayoExists(ensayo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MuestraId"] = new SelectList(_context.Muestras, "Id", "Nombre", ensayo.MuestraId);
+            return View(ensayo);
+        }
+
+        // GET: Ensayos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ensayo = await _context.Ensayos
+                .Include(e => e.Muestra)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ensayo == null)
+            {
+                return NotFound();
+            }
+
+            return View(ensayo);
+        }
+
+        // POST: Ensayos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ensayo = await _context.Ensayos.FindAsync(id);
+            if (ensayo != null)
+            {
+                _context.Ensayos.Remove(ensayo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EnsayoExists(int id)
+        {
+            return _context.Ensayos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ PuntoCuatroSQLite/Migrations/20250906184224_InitialCreateSqlite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PuntoCuatroSQLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateSqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Muestras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", maxLength: 120, nullable: false),
+                    Matriz = table.Column<string>(type: "TEXT", nullable: true),
+                    FechaToma = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Muestras", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ensayos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MuestraId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Tipo = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false),
+                    Resultado = table.Column<string>(type: "TEXT", nullable: true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ensayos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ensayos_Muestras_MuestraId",
+                        column: x => x.MuestraId,
+                        principalTable: "Muestras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ensayos_MuestraId",
+                table: "Ensayos",
+                column: "MuestraId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ensayos");
+
+            migrationBuilder.DropTable(
+                name: "Muestras");
+        }
+    }
+}
