a556fc5bfaccac21456857ca074a312c0b14f8f8
pompone <54503871+pompone@users.noreply.github.com>
2025-09-06 17:58:37 -0300

    correccion

---
+++ PuntoCuatroSQLite/Data/LaboratorioContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PuntoCuatroSQLiteSQLite.Models;
+
+namespace PuntoCuatroSQLiteSQLite.Data
+{
+    public class LaboratorioContext : DbContext
+    {
+        public LaboratorioContext(DbContextOptions<LaboratorioContext> options) : base(options) { }
+
+        public DbSet<Muestra> Muestras { get; set; }
+        public DbSet<Ensayo> Ensayos { get; set; }
+    }
+}
