ca59dce9e68356e1bb9392df4a4c047cd080079f
pompone <54503871+pompone@users.noreply.github.com>
2025-09-06 16:25:43 -0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PuntoCuatro.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
