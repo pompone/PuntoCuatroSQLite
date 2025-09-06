649c923faea72598f842e929d9d120fce0e620fd
pompone <54503871+pompone@users.noreply.github.com>
2025-09-06 16:18:59 -0300

    Tercera subida

---
+++ Data/LaboratorioContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PuntoCuatro.Models;
+
+namespace PuntoCuatro.Data
+{
+    public class LaboratorioContext : DbContext
+    {
+        public LaboratorioContext(DbContextOptions<LaboratorioContext> options) : base(options) { }
+
+        public DbSet<Muestra> Muestras { get; set; }
+        public DbSet<Ensayo> Ensayos { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PuntoCuatro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
